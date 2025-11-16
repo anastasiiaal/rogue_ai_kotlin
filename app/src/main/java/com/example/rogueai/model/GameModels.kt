7728883943250d0c1b863d5d6dd9acc9afaea0cf
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2025-11-16 18:03:28 +0100

    feat: add lobby model

--- app/src/main/java/com/example/rogueai/model/GameModels.kt
+++ app/src/main/java/com/example/rogueai/model/GameModels.kt
@@ -1,2 +1,12 @@
 package com.example.rogueai.model
 
+/**
+ * Models for Lobby screen.
+ */
+
+data class LobbyPlayer(
+    val id: String,
+    val displayName: String,
+    val isReady: Boolean,
+    val isHost: Boolean
+)
