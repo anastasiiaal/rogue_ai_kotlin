eae9333d71d80e555ae754134cce0653c82ffac4
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2025-11-16 18:02:51 +0100

    feat: add room model

--- app/src/main/java/com/example/rogueai/model/RoomModels.kt
+++ app/src/main/java/com/example/rogueai/model/RoomModels.kt
@@ -1,2 +1,13 @@
 package com.example.rogueai.model
 
+data class RoomInfo(
+    val minPlayer: Int,
+    val maxPlayer: Int,
+    val gameDuration: Long,
+    val roomRestriction: String
+)
+
+data class CreateRoomResponse(
+    val roomCode: String,
+    val roomInfo: RoomInfo
+)
