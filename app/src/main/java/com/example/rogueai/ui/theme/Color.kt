fab50e664fdf61adbc9d7100d2255319e7dfb696
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2025-12-27 13:09:09 +0100

    feat: add colors

--- app/src/main/java/com/example/rogueai/ui/theme/Color.kt
+++ app/src/main/java/com/example/rogueai/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+object RoguePalette {
+    val BackgroundTop = Color(0xFF071A3A)
+    val BackgroundBottom = Color(0xFF102B4F)
+    val CardBlue = Color(0xFF1E3557)
+
+    val ButtonYellow = Color(0xFFFFD54F)
+    val ButtonYellowText = Color(0xFF4E342E)
+
+    val ButtonSalmon = Color(0xFFFF8A65)
+    val ButtonSalmonText = Color.White
+
+    val ButtonJoinBlue = Color(0xFF64B5F6)
+    val ButtonJoinText = Color(0xFF0D1B2A)
+}
