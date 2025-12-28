5e08d2f09af70c6a002a089ed00bbd84eb461641
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2025-12-28 18:33:17 +0100

    fix: delete unneeded imports

--- app/src/main/java/com/example/rogueai/ui/game/GameScreen.kt
+++ app/src/main/java/com/example/rogueai/ui/game/GameScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.rogueai.network.RoomSocket
 import com.example.rogueai.ui.game.components.CommandCard
 import org.json.JSONObject
 import androidx.compose.foundation.layout.FlowRow
@@ -23,9 +22,6 @@ import com.example.rogueai.ui.game.components.ThreatBar
 import com.example.rogueai.ui.game.components.InstructionTimer
 import android.util.Log
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.platform.LocalClipboardManager
-import androidx.compose.ui.text.AnnotatedString
 import com.example.rogueai.data.GameRepository
 
 /**
